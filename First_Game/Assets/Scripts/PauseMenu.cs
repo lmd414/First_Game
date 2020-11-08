bd8bf5c4f373e4d9b6c6cca10d47e29dfff88e76
lmd414 <lmd414@msstate.edu>
2020-11-08 14:38:04 -0600

    pause game on esc

---
+++ First_Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static bool GamePaused = false;
+
+    public GameObject PMUI;
+
+    void Start()
+    {
+        PMUI.SetActive(false);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PMUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        PMUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        PMUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+
+        SceneManager.LoadScene("Start");
+    }
+}
